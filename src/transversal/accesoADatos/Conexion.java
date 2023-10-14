28f55b0f3950654843eaa0b1d50947d7aabadfbe
Sergioleguizamon <142453246+Sergioleguizamon@users.noreply.github.com>
2023-10-13 23:08:24 -0300

    añadi la clase Conexion

---
+++ src/transversal/accesoADatos/Conexion.java
@@ -0,0 +1,35 @@
+
+package transversal.accesoADatos;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "transversal";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    private static Connection connection;
+    
+    private Conexion(){
+        
+    }
+    
+    public static Connection getConexion() {
+        if(connection == null){
+            try{
+                Class.forName("org.mariadb.jdbc.Driver");
+                
+                connection = DriverManager.getConnection(URL + DB, USER, PASSWORD);
+                
+                JOptionPane.showMessageDialog(null, "Connectado exitosamente");
+            }catch(ClassNotFoundException ex){
+                JOptionPane.showMessageDialog(null, "Error al cargar driver " + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos");
+            }
+        }
+        return connection;
+    }
+}
---
+++ src/tranversal/Tranversal.java
@@ -0,0 +1,10 @@
+
+package tranversal;
+
+import transversal.accesoADatos.Conexion;
+        
+public class Tranversal {
+    public static void main(String[] args) {
+        Conexion.getConexion();
+   }
+}
